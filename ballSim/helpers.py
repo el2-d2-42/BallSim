74f3fe1b64e4bbddbadf550f73bad39f43019ac6
El2-D2-42 <fdvq38@durham.ac.uk>
2023-10-30 17:27:28 +0000

    Initialises pygame and basic game loop

--- ballSim/helpers.py
+++ ballSim/helpers.py
@@ -1,1 +1,7 @@
 # Any helpers will be placed in this file
+
+#Generates a random 2d coordinate (vector from the origin)
+def randomXY(maxX = 0, minX = 0, maxY = 0, minY = 0):
+    
+    # ...
+    return
